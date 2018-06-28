e5d5d9c421d7ce007d098596c40d225be7d5ee0b
Chris Burgin <chrisburgin95@gmail.com>
2018-06-28 16:21:14 -0400

    INIT

--- README.md
+++ README.md
@@ -2,12 +2,6 @@
 
 ## Reset Your Project
 
-## Array Functions
+## Using Components
 
-- Array.map
-- Array.filter
-- Array.find
-
-## Printing an Array in JSX
-
-## State in React, constuctor method
+## Updating State
--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      people: [
+        { name: "Sally W.", age: 23 },
+        { name: "Rick S.", age: 30 },
+        { name: "Samantha T.", age: 52 }
+      ]
+    };
+  }
+
   render() {
+    const { people } = this.state;
     return (
       <div className="App">
-        <h1> Hello World </h1>
-        <p> This is my cool hello world project. </p>
-        <p className="red"> This text is RED </p>
+        <h1> People - Names </h1>
+        <ul className="people">
+          {people.map(person => (
+            <li className="person">
+              <input type="text" value={person.name} />
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
