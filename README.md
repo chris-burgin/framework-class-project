95ee891aa0358c6e88eeb8d98c3f35017b7cee9c
Chris Burgin <chrisburgin95@gmail.com>
2018-06-28 16:08:48 -0400

    INIT

---
+++ README.md
@@ -0,0 +1,9 @@
+# Week 1
+
+## Installing Node
+
+## Clone and Install
+
+## Starting The Project
+
+## Hello World
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div className="App">{/*  Say Hello World Here */}</div>;
+  }
+}
+
+export default App;
