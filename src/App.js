fa0b45a03bed4b4ffac5f23acd4890d4ef94c47f
Chris Burgin <chrisburgin95@gmail.com>
2018-06-28 16:10:01 -0400

    Example

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div className="App">{/*  Say Hello World Here */}</div>;
+    return (
+      <div className="App">
+        <h1> Hello World </h1>
+        <p> This is my cool hello world project. </p>
+        <p className="red"> This text is RED </p>
+      </div>
+    );
   }
 }
 
